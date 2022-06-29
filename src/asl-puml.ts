848b3a915014d9726c26d798d792a618df94d96d
markford <mark@massfords.com>
2022-06-29 08:28:39 -0400

    fix: restore asl-validator call with assert guard

--- src/asl-puml.ts
+++ src/asl-puml.ts
@@ -1,18 +1,15 @@
 import { AslDefinition } from "./lib/types";
-// import { validator } from "./lib/asl-validator";
 import { theme } from "./lib/theme";
 import { decls } from "./lib/decls";
 import { footer } from "./lib/footer";
 import { build_state_map } from "./lib/build_state_map";
 import { transitions } from "./lib/transitions";
+import { aslValidator } from "./lib/validator";
 
 export const asl_to_puml = (
   definition: AslDefinition
 ): { isValid: true; puml: string } | { isValid: false; message: string } => {
-  // const { isValid, errorsText } = validator(definition);
-  // if (!isValid) {
-  //   return { isValid: false, message: errorsText("\n") };
-  // }
+  aslValidator(definition);
   const state_map = build_state_map(definition);
 
   // we know the definition is valid at this point
---
+++ src/lib/validator.ts
@@ -0,0 +1,20 @@
+import { AslDefinition } from "./types";
+
+export type ValidatorFunction = (definition: AslDefinition) => {
+  isValid: boolean;
+  errorsText: (sep: string) => string;
+};
+
+const validator: ValidatorFunction =
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  require("asl-validator") as ValidatorFunction;
+
+export type ASLValidator = (definition: AslDefinition) => asserts definition;
+export const aslValidator: ASLValidator = (
+  definition: AslDefinition
+): asserts definition => {
+  const result = validator(definition);
+  if (!result.isValid) {
+    throw Error(`definition is invalid:${result.errorsText("\n")}`);
+  }
+};
