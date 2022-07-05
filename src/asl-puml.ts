11eeb0481c1c5ce34982f7b838930bc14ddb3fb8
Mark Ford <mark@massfords.com>
2022-07-05 13:47:18 -0400

    feat: added config (#12)

--- src/asl-puml.ts
+++ src/asl-puml.ts
@@ -1,22 +1,30 @@
-import { AslDefinition } from "./lib/types";
+import { AslDefinition, UserSpecifiedConfig } from "./lib/types";
 import { theme } from "./lib/theme";
 import { decls } from "./lib/decls";
 import { footer } from "./lib/footer";
 import { build_state_map } from "./lib/build_state_map";
 import { transitions } from "./lib/transitions";
 import { aslValidator } from "./lib/validator";
+import { toConfig } from "./lib/config";
 
 export const asl_to_puml = (
-  definition: AslDefinition
+  definition: AslDefinition,
+  userSpecifiedConfig: UserSpecifiedConfig
 ): { isValid: true; puml: string } | { isValid: false; message: string } => {
   aslValidator(definition);
-  const state_map = build_state_map(definition);
 
-  // we know the definition is valid at this point
-  let puml = theme(definition, state_map);
-  puml += decls(definition, state_map);
-  puml += transitions(definition, state_map);
-  puml += footer(definition, state_map);
+  const response = toConfig(userSpecifiedConfig);
+  if (!response.isValid) {
+    return { isValid: false, message: response.message };
+  }
+  const { config } = response;
+
+  const state_map = build_state_map(definition, config);
+
+  let puml = theme(definition, state_map, config);
+  puml += decls(definition, state_map, config);
+  puml += transitions(definition, state_map, config);
+  puml += footer(definition, state_map, config);
 
   return { isValid: true, puml };
 };
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -37,7 +37,117 @@ export interface AslChoiceTransitionNode {
   StringEquals?: string;
 }
 
+export type Color = `#${string}`;
+export interface StateConfig {
+  BackgroundColor: Color;
+  FontColor?: Color | "automatic";
+}
+export interface ContainerStateConfig {
+  BackgroundColor: Color;
+}
+export interface LineConfig {
+  bold?: true;
+  color: Color;
+}
+export interface CompensationConfig {
+  pattern: RegExp | string;
+  color: Color;
+}
+export interface UserSpecifiedConfig {
+  theme: {
+    compensation?: CompensationConfig;
+    lines?: {
+      fromCatch?: LineConfig;
+      toFail?: LineConfig;
+    };
+    skinparams?: {
+      ArrowColor: Color;
+    };
+    states?: {
+      Pass?: StateConfig;
+      Task?: StateConfig;
+      Choice?: StateConfig;
+      Wait?: StateConfig;
+      Succeed?: StateConfig;
+      Fail?: StateConfig;
+      Parallel?: ContainerStateConfig;
+      Map?: ContainerStateConfig;
+    };
+  };
+}
+
+export interface Config {
+  theme: {
+    compensation: CompensationConfig;
+    lines: {
+      fromCatch: LineConfig;
+      toFail: LineConfig;
+    };
+    skinparams: {
+      ArrowColor: Color;
+    };
+    states: {
+      Choice: StateConfig;
+      Fail: StateConfig;
+      Map: StateConfig;
+      Parallel: StateConfig;
+      Pass: StateConfig;
+      Succeed: StateConfig;
+      Task: StateConfig;
+      Wait: StateConfig;
+    };
+  };
+}
+
+export const DefaultConfig: Config = {
+  theme: {
+    skinparams: {
+      ArrowColor: "#black",
+    },
+    states: {
+      Pass: {
+        BackgroundColor: "#whitesmoke",
+      },
+      Map: {
+        BackgroundColor: "#whitesmoke",
+      },
+      Choice: {
+        BackgroundColor: "#whitesmoke",
+      },
+      Parallel: {
+        BackgroundColor: "#whitesmoke",
+      },
+      Wait: {
+        BackgroundColor: "#whitesmoke",
+      },
+      Task: {
+        BackgroundColor: "#lightblue",
+      },
+      Fail: {
+        BackgroundColor: "#red",
+      },
+      Succeed: {
+        BackgroundColor: "#green",
+      },
+    },
+    lines: {
+      fromCatch: {
+        bold: true,
+        color: "#orange",
+      },
+      toFail: {
+        color: "#pink",
+      },
+    },
+    compensation: {
+      pattern: "^.*(compensate).*$",
+      color: "#orange",
+    },
+  },
+};
+
 export type PumlBuilder = (
   definition: AslDefinition,
-  state_map: Map<StateName, StateHints>
+  state_map: Map<StateName, StateHints>,
+  config: Config
 ) => string;
